a96fe7ea4d3e5c9139ad4a81ee9e9a2b06b0111c
Rodrigo Reyes <rsanchez@vectorworks.net>
2025-09-15 19:27:15 -0400

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxtjs/tailwindcss',
+    'v-gsap-nuxt'
+  ]
+})
